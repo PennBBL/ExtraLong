67ffa52884112da905e2b2ba1943ec0b623b073a
butellyn <ellynrbutler@gmail.com>
2020-10-02 16:47:23 -0400

    Establish fmriprep and freesurfer directory structure

--- scripts/organize/downloadfMRIPrep.py
+++ scripts/organize/downloadfMRIPrep.py
@@ -15,6 +15,8 @@ import time
 import pandas as pd
 import pytz
 import datetime
+import zipfile
+import numpy as np
 
 
 fw = flywheel.Client()
@@ -46,19 +48,65 @@ def get_latest_fmriprep_correctversion(session, fmriprepVersion):
     else:
         return None
 
-fmriprepdir = '/Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprepdir/'
+outdir = '/Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/'
+if not os.path.exists(outdir+'fmriprep'):
+    os.mkdir(outdir+'fmriprep')
+if not os.path.exists(outdir+'freesurfer'):
+    os.mkdir(outdir+'freesurfer')
+
+
 #for subj in subjects:
 subj = subjects[0]
 sublabel = subj['label']
-os.mkdir(fmriprepdir+sublabel)
+#if not os.path.exists(fmriprepdir+sublabel):
+#    os.mkdir(fmriprepdir+sublabel)
 for ses in subj.sessions():
     ses = ses.reload()
     d = get_latest_fmriprep_correctversion(ses, '0.3.4_20.0.5')
     # Get file name
     filename = [s for s in d['job']['saved_files'] if 'fmriprep_sub-' in s][0]
     seslabel = ses['label']
-    os.mkdir(fmriprepdir+sublabel+'/'+seslabel)
-    d.download_file(filename, fmriprepdir+sublabel+'/'+seslabel+'/'+filename)
+    # Check if the fmriprep and freesurfer dirs are already in the ses dir
+    if not os.path.exists(outdir+'fmriprep/'+sublabel+'/'+seslabel):
+        d.download_file(filename, outdir+filename)
+        # Unzip files
+        with zipfile.ZipFile(outdir+filename,"r") as zip_ref:
+            zip_ref.extractall(outdir)
+        # Get rid of id directory and zip
+        iddir = [dI for dI in os.listdir(outdir) if os.path.isdir(os.path.join(outdir, dI))]
+        iddir = np.setdiff1d(iddir, ['fmriprep', 'freesurfer'])[0]
+        for processeddir in ['fmriprep', 'freesurfer']:
+            source = outdir+iddir+'/'+processeddir
+            files = os.listdir(source) #######
+            notsubdirfiles = []
+            for dI in os.listdir(outdir+iddir+'/'+processeddir):
+                if os.path.isdir(os.path.join(outdir+iddir+'/'+processeddir, dI)) and 'sub-' in dI:
+                    subdir = dI
+                else:
+                    notsubdirfiles.append(dI)
+            # Move contents of session-specific fmriprep dir to a session directory
+            # within the fmriprep-created subject directory.
+            os.mkdir(source+'/'+subdir+'/'+seslabel)
+            for file in notsubdirfiles:
+                shutil.move(f"{source}/{file}", outdir+iddir+'/'+processeddir+'/'+sublabel+'/'+seslabel)
+            # Move anat and figures into ses dir
+            for dI in os.listdir(outdir+iddir+'/'+processeddir+'/'+subdir):
+                if not 'ses-' in dI:
+                    shutil.move(f"{source}/{dI}", outdir+iddir+'/'+processeddir+'/'+sublabel+'/'+seslabel)
+            # Collapse directories
+            if processeddir == 'fmriprep':
+                # Move this sub*/ses* directory into the fmriprep directory that I
+                # created
+            else:
+                # Move the contents of ses dir into the fmriprep ses dir
+            # Delete the directory with the analysis ID as the name
+            os.rmdir(source)
+        os.remove(outdir+sublabel+'/'+seslabel+'/'+filename)
+    if not :
+        # Rename fmriprep output to include the session label
+        fmriprep_files = os.listdir()
+    if not os.path.exists(outdir+sublabel+'/'+seslabel):
+        os.mkdir(outdir+sublabel+'/'+seslabel)
 
 
 
@@ -67,10 +115,7 @@ for ses in subj.sessions():
 
 
 
-
-
-
-
+# TO DO: Add lines to check if directories and files already exist before creating them
 
 
 
