29d3d6a8fd84344d48927c8954e4a45106b1b8f0
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-04-19 10:43:42 -0400

    Edited to filter out bad scans, based on antssstExclude.csv

--- scripts/process/FreesurferLong/make_avg_templates.sh
+++ scripts/process/FreesurferLong/make_avg_templates.sh
@@ -3,19 +3,19 @@
 export LOGS_DIR=/home/kzoner/logs/ExtraLong/fs-base
 
 project=/project/ExtraLong
-input_dir=${project}/data/bids_directory
+exclude_csv=${project}/data/qualityAssessment/antssstExclude.exclude_csv
 output_dir=${project}/data/freesurferLongitudinal/fs-base
 
 mkdir -p ${project}/scripts/jobscripts/fs-base
 
-subjList=`ls -d ${project}/data/bids_directory/sub-* | cut -d / -f 6`
+subjList=`cat ${exclude_csv} | grep FALSE | cut -d , -f 1 | uniq`
 
 for subject in $subjList; do
-	sessList=`ls -d ${project}/data/bids_directory/${subject}/ses-* | cut -d / -f 7`
+	sessList=`cat ${exclude_csv} | grep ${subject} | grep FALSE | cut -d , -f 2 | cut -d \" -f 2`
     
     	tpArgs=""
     	for session in $sessList; do 
-        	tpArgs="${tpArgs} -tp ${session}"; 
+        	tpArgs="${tpArgs} -tp ses-${session}"; 
     	done
 
     	jobscript=${project}/scripts/jobscripts/fs-base/${subject}.sh
@@ -26,7 +26,7 @@ for subject in $subjList; do
 		module load freesurfer/7.1.1
 		export FREESURFER_HOME=/appl/freesurfer-7.1.1
 		source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
-		SURFER_FRONTDOOR=1 ${FREESURFER_HOME}/bin/recon-all -base ${subject} ${tpArgs} -all
+		SURFER_FRONTDOOR=1 ${FREESURFER_HOME}/bin/recon-all -base sub-${subject} ${tpArgs} -all
 	EOS
 	break
 	chmod +x ${jobscript}
