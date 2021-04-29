897525d142d6f74cbea2de9c9d1aed5364f0813b
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-04-29 13:38:20 -0400

    Fixed spacing issues.

--- scripts/process/FreesurferLong/run_longitudinal.sh
+++ scripts/process/FreesurferLong/run_longitudinal.sh
@@ -5,7 +5,6 @@ export LOGS_DIR=/home/kzoner/logs/ExtraLong/fs-long
 
 project=/project/ExtraLong
 exclude_csv=${project}/data/qualityAssessment/antssstExclude.csv
-input_dir=${project}/data/bids_directory
 output_dir=${project}/data/freesurferLongitudinal
 
 mkdir -p ${project}/scripts/jobscripts/fs-long
@@ -13,11 +12,12 @@ mkdir -p ${project}/scripts/jobscripts/fs-long
 subjList=`cat ${exclude_csv} | grep FALSE | cut -d , -f 1 | uniq`
 
 for subj in $subjList; do
-	
-	sessList=`cat ${exclude_csv} | grep ${subj} | grep FALSE | cut -d , -f 2 | cut -d \" -f 2`
+
+    sessList=`cat ${exclude_csv} | grep ${subj} | grep FALSE | cut -d , -f 2 | cut -d \" -f 2`
     
-    for sess in $sessList; do 
-	    echo SUBJECT: sub-${subj} SESSION: ses-${sess}
+    for sess in $sessList; do 	
+	
+	echo SUBJECT: sub-${subj} SESSION: ses-${sess}
         
         # Skip session if does not exist in subject's FS output dir
         if [ ! -d "${output_dir}/sub-${subj}/ses-${sess}" ]; then
@@ -27,7 +27,7 @@ for subj in $subjList; do
 
         jobscript=${project}/scripts/jobscripts/fs-long/sub-${subj}_ses-${sess}.sh
 
-cat <<- EOS > ${jobscript}
+	cat <<- EOS > ${jobscript}
             #!/bin/bash
             
             module load freesurfer/7.1.1
@@ -35,8 +35,9 @@ cat <<- EOS > ${jobscript}
             source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
             export SUBJECTS_DIR=${output_dir}/sub-${subj}
             SURFER_FRONTDOOR=1 ${FREESURFER_HOME}/bin/recon-all -long ses-${sess} Template-${subj} -all
-EOS
-        break
+	EOS
+        
+	break
         chmod +x ${jobscript}
         bsub -e $LOGS_DIR/sub-${subj}_ses-${sess}.e -o $LOGS_DIR/sub-${subj}_ses-${sess}.o ${jobscript}
     done
