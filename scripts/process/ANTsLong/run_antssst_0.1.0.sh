211b95f511df5a422fc48c2bfde3b8b38decc642
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-07-13 11:19:27 -0600

    Launch script to run ANTsSST v0.1.0 on the 8 subjects that will currently make up the group template.

--- scripts/process/ANTsLong/run_antssst_0.1.0.sh
+++ scripts/process/ANTsLong/run_antssst_0.1.0.sh
@@ -8,17 +8,19 @@ mkdir -p ${jsDir}
 
 fmriprep_dir=/project/ExtraLong/data/freesurferCrossSectional/fmriprep
 antssst_dir=~/ants_pipelines/data/singleSubjectTemplates/antssst-0.1.0
+atlas_dir=~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases
 
-exclude_csv="/project/ExtraLong/data/qualityAssessment/antssstExclude.csv"
+gt_subs_csv=~/ants_pipelines/subjects_for_gt.csv
+exclude_csv=/project/ExtraLong/data/qualityAssessment/antssstExclude.csv
 
-#subList=`ls ${fmriprep_dir}`
-subjList=`cat ${exclude_csv} | grep FALSE | cut -d , -f 1 | uniq`
+subjList=`cat ${gt_subs_csv} | cut --d - -f 2`
+#subjList=`cat ${exclude_csv} | grep FALSE | cut -d , -f 1 | uniq`
 
 for subject in $subList; do
 
-        #echo SUBJECT: $subject
+        echo SUBJECT: $subject
 	sessions=`cat ${exclude_csv} | grep ${subject} | grep FALSE | cut -d , -f 2 | cut -d \" -f 2`
-
+        echo SESSIONS: $sessions
 
         out_dir=${antssst_dir}/${subject}
         mkdir -p ${out_dir}
@@ -31,13 +33,13 @@ for subject in $subList; do
                 singularity run --writable-tmpfs --cleanenv --containall \\
 		-B ${fmriprep_dir}/${subject}:/data/input/fmriprep \\
 		-B ${out_dir}:/data/output \\
-		-B ~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases:/data/input/mindboggleVsBrainCOLOR_Atlases \\
+		-B ${atlas_dir}:/data/input/atlases \\
 		~/ants_pipelines/images/antssst_0.1.0.sif --seed 1 --jlf ${sessions}
 
         JOBSCRIPT
 
         chmod +x ${jobscript}
 
-        bsub -e $LOGS_DIR/${subject}.e -o $LOGS_DIR/${subject}.o ${jobscript}
+        #bsub -e $LOGS_DIR/${subject}.e -o $LOGS_DIR/${subject}.o ${jobscript}
 
 done
