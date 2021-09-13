24696d9f381c05c721779a492297e5ed3fe83761
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-09-13 13:12:39 -0400

    Fixed bug where sessions were newline separated in heredoc

--- scripts/process/ANTsLong/run_antssst_0.1.0.sh
+++ scripts/process/ANTsLong/run_antssst_0.1.0.sh
@@ -3,7 +3,7 @@
 export LOGS_DIR=/home/kzoner/logs/ants/antssst-0.1.0
 mkdir -p ${LOGS_DIR}
 
-jsDir=~/ants_pipelines/data/scripts/jobscripts/antssst-0.1.0
+jsDir=~/ants_pipelines/scripts/jobscripts/antssst-0.1.0
 mkdir -p ${jsDir}
 
 fmriprep_dir=/project/ExtraLong/data/freesurferCrossSectional/fmriprep
@@ -13,14 +13,15 @@ atlas_dir=~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases
 gt_subs_csv=~/ants_pipelines/subjects_for_gt.csv
 exclude_csv=/project/ExtraLong/data/qualityAssessment/antssstExclude.csv
 
-subjList=`cat ${gt_subs_csv} | cut --d - -f 2`
+subList=`cat ${gt_subs_csv} | cut -d - -f 2`
 #subjList=`cat ${exclude_csv} | grep FALSE | cut -d , -f 1 | uniq`
 
 for subject in $subList; do
 
-        echo SUBJECT: $subject
-	sessions=`cat ${exclude_csv} | grep ${subject} | grep FALSE | cut -d , -f 2 | cut -d \" -f 2`
-        echo SESSIONS: $sessions
+	sessions=`cat ${exclude_csv} | grep ${subject} | grep FALSE | cut -d \" -f 2 | sed "s/^/ses-/" | tr "\n" " "`
+	subject=sub-${subject}	
+	echo SUBJECT: $subject
+	echo SESSIONS: $sessions
 
         out_dir=${antslong_dir}/subjects/${subject}
         mkdir -p ${out_dir}
@@ -28,18 +29,17 @@ for subject in $subList; do
         jobscript=${jsDir}/${subject}.sh
 
         cat <<- JOBSCRIPT > ${jobscript}
-                #!/bin/bash
-
-                singularity run --writable-tmpfs --cleanenv --containall \\
-		-B ${fmriprep_dir}/${subject}:/data/input/fmriprep \\
-		-B ${out_dir}:/data/output \\
-		-B ${atlas_dir}:/data/input/atlases \\
-		~/ants_pipelines/images/antssst_0.1.0.sif --seed 1 ${sessions}
-
-        JOBSCRIPT
+		#!/bin/bash
+	
+		singularity run --writable-tmpfs --cleanenv --containall \\
+			-B ${fmriprep_dir}/${subject}:/data/input/fmriprep \\
+			-B ${out_dir}:/data/output \\
+			-B ${atlas_dir}:/data/input/atlases \\
+			~/ants_pipelines/images/antssst_0.1.0.sif --seed 1 ${sessions}
+	
+	JOBSCRIPT
 
         chmod +x ${jobscript}
-
-        #bsub -e $LOGS_DIR/${subject}.e -o $LOGS_DIR/${subject}.o ${jobscript}
+        bsub -e $LOGS_DIR/${subject}.e -o $LOGS_DIR/${subject}.o ${jobscript}
 
 done
