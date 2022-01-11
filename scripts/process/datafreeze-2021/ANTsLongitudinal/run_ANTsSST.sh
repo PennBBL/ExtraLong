66a514d759eaeaf28187209a18be6b6817105a8e
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2022-01-11 10:53:24 -0500

    Using new inclusion file. Changed dir paths. Removed --containall flag

--- scripts/process/datafreeze-2021/ANTsLongitudinal/run_ANTsSST.sh
+++ scripts/process/datafreeze-2021/ANTsLongitudinal/run_ANTsSST.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-export LOGS_DIR=/home/kzoner/logs/ExtraLong/ANTsSST-0.1.0
+export LOGS_DIR=/home/kzoner/logs/ExtraLong_2021/ANTsSST-0.1.0
 mkdir -p ${LOGS_DIR}
 
 scripts="/project/ExtraLong/scripts/process/datafreeze-2021/ANTsLongitudinal"
 jsDir=${scripts}/jobscripts
 mkdir -p ${jsDir}
 
-data="/project/ExtraLong/data/datafreeze-2021/"
-fmriprep_dir=${data}/fmriprep/
+data="/project/ExtraLong/data/datafreeze-2021"
+fmriprep_dir=${data}/fmriprep
 antslong_dir=${data}/ANTsLongitudinal
-atlas_dir=${data}/../mindboggleVsBrainCOLOR_Atlases
+atlas_dir="/project/ExtraLong/data/mindboggleVsBrainCOLOR_Atlases"
 
-# gt_subs_csv=~/ants_pipelines/subjects_for_gt.csv
-# exclude_csv=/project/ExtraLong/data/qualityAssessment/antssstExclude.csv
-exclude_csv="/project/ExtraLong/data/datafreeze-2021/QC/exclusion_datafreeze-2021_cutoff-212.csv"
-
-subList=$(cat ${exclude_csv} | grep False | cut -d , -f 1 | uniq)
+include_csv=${data}/QC/sessions_for_inclusion.csv
+subList=$(cat ${include_csv} | cut -d , -f 1 | uniq)
+subList=$(tail ${LOGS_DIR}/failed.txt)
 echo "ANTsSST will be run on $(echo $subList | wc -w) subjects"
 
 for subject in $subList; do
 
-	sessions=$(cat ${exclude_csv} | grep ${subject} | grep False | cut -d , -f 2 | sed "s/^/ses-/" | tr "\n" " ")
+	sessions=$(cat ${include_csv} | grep ${subject} | cut -d , -f 2 | sed "s/^/ses-/" | tr "\n" " ")
 	subject=sub-${subject}
 	echo SUBJECT: $subject
 	echo SESSIONS: $sessions
@@ -34,7 +32,7 @@ for subject in $subList; do
 	cat <<-JOBSCRIPT >${jobscript}
 		#!/bin/bash
 
-		singularity run --writable-tmpfs --cleanenv --containall \\
+		singularity run --writable-tmpfs --cleanenv  \\
 			-B ${fmriprep_dir}/${subject}:/data/input/fmriprep \\
 			-B ${out_dir}:/data/output \\
 			-B ${atlas_dir}:/data/input/atlases \\
@@ -44,5 +42,4 @@ for subject in $subList; do
 
 	chmod +x ${jobscript}
 	bsub -e $LOGS_DIR/${subject}.e -o $LOGS_DIR/${subject}.o ${jobscript}
-
 done
